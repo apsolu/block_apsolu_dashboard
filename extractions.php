7e00e56cb831efefc7d93c615311c09c702ab9aa
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-06-15 17:14:24 +0200

    refactor(core): supprime une classe inutilisée

--- extractions.php
+++ extractions.php
@@ -24,7 +24,6 @@
 
 require_once(__DIR__.'/../../config.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
-require_once($CFG->dirroot.'/blocks/apsolu_dashboard/locallib.php');
 require_once($CFG->dirroot.'/blocks/apsolu_dashboard/extractions_form.php');
 require_once($CFG->libdir.'/excellib.class.php');
 
--- shnu.php
+++ shnu.php
@@ -26,7 +26,6 @@ define('SHNUID', 423);
 
 require_once(__DIR__.'/../../config.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
-require_once($CFG->dirroot.'/blocks/apsolu_dashboard/locallib.php');
 require_once($CFG->dirroot.'/blocks/apsolu_dashboard/shnu_form.php');
 require_once($CFG->libdir.'/excellib.class.php');
 
